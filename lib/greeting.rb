de1d9836f847dcd4c79b6b22c2d90b2ecabca33d
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-22 22:15:31 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,11 @@
 # code the #greeting method here!
+
+def greeting(name)
+  puts "Hello, " + name + ". It's nice to meet you"
+end
+
+greeting("Bobby")
+
+
+
+#The `lib` directory is where we place the code that our program relies on to run. It is the meat of our CLI application. Our #executable file *requires* the files in the `lib` directory and uses the code (for example, calling on any methods we might #define) to enact the CLI.

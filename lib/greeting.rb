082363472c6ae24e0d4d6a3f19ac23c7b546826a
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-22 22:37:40 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -6,8 +6,6 @@ end
 
 
 
+
 # greeting("Bobby")
-
-
-
-#The `lib` directory is where we place the code that our program relies on to run. It is the meat of our CLI application. Our #executable file *requires* the files in the `lib` directory and uses the code (for example, calling on any methods we might #define) to enact the CLI.
+# The `lib` directory is where we place the code that our program relies on to run. It is the meat of our CLI application. Our #executable file *requires* the files in the `lib` directory and uses the code (for example, calling on any methods we might #define) to enact the CLI.

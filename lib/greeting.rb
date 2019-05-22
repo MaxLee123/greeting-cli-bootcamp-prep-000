1285633c0d393224aab6d098d187d2c77af67140
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-22 22:37:57 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,10 +1,12 @@
 # code the #greeting method here!
 
 def greeting(name)
-  puts "Hello, #{name}. It's nice to meet you"
+  puts "Hello, #{name}. It's nice to meet you."
 end
 
-
+def greeting(name)
+  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+end
 
 
 # greeting("Bobby")

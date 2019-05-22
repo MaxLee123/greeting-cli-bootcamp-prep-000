99823f34d56a2fc50e1254afcd66885a0a702184
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-22 22:38:05 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -4,10 +4,6 @@ def greeting(name)
   puts "Hello, #{name}. It's nice to meet you."
 end
 
-def greeting(name)
-  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
-end
-
 
 # greeting("Bobby")
 # The `lib` directory is where we place the code that our program relies on to run. It is the meat of our CLI application. Our #executable file *requires* the files in the `lib` directory and uses the code (for example, calling on any methods we might #define) to enact the CLI.

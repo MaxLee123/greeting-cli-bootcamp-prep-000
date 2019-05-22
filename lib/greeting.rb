520e550394b1c658eaf53fe17322182f89c4e23f
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-22 22:17:37 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -4,6 +4,10 @@ def greeting(name)
   puts "Hello, " + name + ". It's nice to meet you"
 end
 
+
+def greeting(name)
+  puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
+end
 greeting("Bobby")
 
 
